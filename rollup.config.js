2ec5d78cf83ee9781b51abaef5b19c0eb0290d25
zakarialaoui10 <zakarialaoui10@gmail.com>
2024-03-20 01:41:32 +0000

    setup

---
+++ rollup.config.js
@@ -0,0 +1,36 @@
+
+import commonjs from '@rollup/plugin-commonjs';
+import resolve from '@rollup/plugin-node-resolve';
+import babel from '@rollup/plugin-babel';
+
+const banner= `
+/*
+  Project: react-ziko
+  Author: Zakaria Elalaoui
+  Date : ${new Date()}
+  Git-Repo : https://github.com/zakarialaoui10/react-ziko.js
+  Released under MIT License
+*/
+`
+export default {
+  input: 'src/index.jsx',
+  output: {
+    file: 'dist/react-ziko.js',
+    format: 'umd',
+    name: 'ZikoUI',
+    globals: {
+      react: 'React',
+      ziko : 'Ziko'
+    },
+    banner
+  },
+  plugins: [
+    resolve(),
+    commonjs(),
+    babel({
+      babelHelpers: 'bundled',
+      presets: ['@babel/preset-env', '@babel/preset-react'],
+    }),
+  ],
+  external: ['react','ziko'],
+};
