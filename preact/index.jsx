36a53641625cd879149388edcb15c71f70d00538
zakarialaoui10 <zakarialaoui10@gmail.com>
2024-09-05 12:33:49 +0100

    fix preact wrapper

--- preact/index.jsx
+++ preact/index.jsx
@@ -1,15 +1,15 @@
-import React, { useRef, useEffect } from "preact/hooks";
+import  {useRef, useEffect } from "preact/hooks";
 import {ZikoUIElement} from "ziko";
-function ZikoUI({ ui }) {
-  const containerRef = useRef(null);
-  useEffect(() => {
-    if (containerRef.current && ui && ui instanceof ZikoUIElement) {
-      containerRef.current.innerHTML = "";
-      containerRef.current.appendChild(ui.element);
-    }
+export default function ZikoUI({ui}){
+    const containerRef = useRef(null);
+    useEffect(() => {
+        if (containerRef.current && ui && ui instanceof ZikoUIElement) {
+            ui.unrender()
+            containerRef.current.innerHTML = "";
+            containerRef.current.appendChild(ui.element);
+        }
   }, [ui]);
-  return (
-      <div ref={containerRef} data-engine="ziko"></div>
-  );
+    return (
+        <div ref={containerRef} data-engine="ziko"></div>
+    )
 }
-export default ZikoUI;
