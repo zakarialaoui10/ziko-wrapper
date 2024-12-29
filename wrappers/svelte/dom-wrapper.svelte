eedb3c9e03634d790cd09139becf39162088f4a2
zakarialaoui10 <zakarialaoui10@gmail.com>
2024-12-29 22:58:40 +0100

    fix svelte

---
+++ wrappers/svelte/dom-wrapper.svelte
@@ -0,0 +1,41 @@
+<script>
+    import { onMount } from 'svelte';
+    export let ui; 
+    export let wrapper;
+    export let engine;
+    
+    let containerRef;
+    $: Attributes = {
+        ...(wrapper && { 'data-wrapper': wrapper }),
+        ...(engine && { 'data-engine': engine })
+    };
+    onMount(() => {
+      if(containerRef){
+        containerRef.innerHTML = "";  
+        if(ui instanceof HTMLElement){
+          containerRef.appendChild(ui); 
+        }
+        else if(ui instanceof Array){
+          ui.forEach(item => containerRef.appendChild(item));
+        }
+        console.warn("UI element is not an instance of HTMLElement.");
+      }
+    });
+  </script>
+  
+  <div 
+    bind:this={containerRef} 
+    data-wrapper={wrapper}
+    data-engine={engine}
+    {...Attributes}
+    style="display: contents;"
+  ></div>
+
+  <!-- <div 
+    bind:this={containerRef} 
+    data-wrapper={wrapper}
+    data-engine={engine}
+    style="display: contents;"
+  ></div> -->
+  
+  
