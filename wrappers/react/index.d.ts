2f40ecd3fdf71559c50faa676b4968e19adc8ebb
zakarialaoui10 <zakarialaoui10@gmail.com>
2024-12-30 15:37:18 +0100

    fix react types

--- wrappers/react/index.d.ts
+++ wrappers/react/index.d.ts
@@ -1,4 +1,4 @@
-import { ReactNode, RefObject, ReactElement, FunctionComponent, ComponentClass } from "react";
+import { ReactNode, RefObject, ReactElement, FunctionComponent, ComponentClass, JSX } from "react";
 import { ZikoUIElement } from "ziko";
 
 interface WrapperProps {
@@ -9,14 +9,6 @@ export function ZikoWrapper({ children }: WrapperProps): JSX.Element;
 
 export declare const containerRef: RefObject<HTMLDivElement>;
 
-/**
- * A wrapper for React components to render them into a ZikoUIElement container.
- * Supports both JSX and function invocation with props.
- *
- * @param Component - A React component, either as JSX or a component function/class.
- * @param props - Props to pass if the component is a function or class (not JSX).
- * @returns A ZikoUIElement wrapping the rendered React component.
- */
 export declare function ReactWrapper(
     Component: ReactElement | FunctionComponent<any> | ComponentClass<any>,
     props?: Record<string, any>
