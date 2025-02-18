0749f50e555f20f5b87393c00ad093f6ccdccc6d
zakarialaoui10 <zakarialaoui10@gmail.com>
2025-02-18 23:47:13 +0100

    add client.d.ts

---
+++ wrappers/astro/client.d.ts
@@ -0,0 +1,1 @@
+export default function (wrapper: HTMLElement): (Component: any, props: Record<string, any>, { default: children, ...slotted }: Record<string, any>, { client }: Record<string, string>) => void;
