1b1f40ce6ec5e1bd8075e519d608d95058c57ac1
zakarialaoui10 <zakarialaoui10@gmail.com>
2025-07-10 14:16:39 +0100

    setup ziko-van interleaving approach

--- examples/van-ziko/src/App.js
+++ examples/van-ziko/src/App.js
@@ -1,14 +1,18 @@
 import van from "vanjs-core";
 import { Flex } from "ziko";
+import { Activity } from "vanjs-lucide";
 import "ziko-wrapper/van";
+
 
 const { p } = van.tags;
 
 const pp = p(
     Flex(
-        p("Hello From Van")
+        p("Hello From Van"),
+        Activity({ class:"icon", style:"color: turquoise" })
     )
     .style({ color: "red" })
+    .vertical(0, 0)
 );
 
 document.body.append(pp)
