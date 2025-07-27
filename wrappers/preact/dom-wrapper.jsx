e3d29e3b5e5c3ebc430882f44400a406f7969726
zakarialaoui10 <zakarialaoui10@gmail.com>
2025-07-27 09:35:59 +0100

    fix dom-wrapper

--- wrappers/preact/dom-wrapper.jsx
+++ wrappers/preact/dom-wrapper.jsx
@@ -1,24 +1,46 @@
 import { useRef, useEffect } from "preact/hooks";
-export function DomWrapper({ children, engine, wrapper}) {
-    const containerRef = useRef(null);
-    useEffect(() => {
-        if (containerRef.current) {
-            containerRef.current.innerHTML = "";             
-            const childrenArray = Array.isArray(children) ? children : [children];
-            childrenArray.forEach((child) => {
-                const {type, props} = child
-                const element = type.call(this, props)
-                if (element instanceof HTMLElement) containerRef.current.appendChild(element);
-                
-            });
+
+const processDomComponent = (child) => {
+  if (typeof child?.type === "function") {
+    let DomComponent;
+    if (child.props?.children) {
+      const childArray = Array.isArray(child.props.children)
+        ? child.props.children
+        : [child.props.children];
+      const processedChildren = childArray
+        .map((nestedChild) => processDomComponent(nestedChild))
+        .filter((element) => element !== null);
+      DomComponent = child.type(child.props, ...processedChildren);
+    } else {
+      DomComponent = child.type(child.props);
+    }
+    return DomComponent;
+  }
+  return null;
+};
+
+export function ZikoWrapper({ children, engine, wrapper}) {
+  const containerRef = useRef(null);
+
+  useEffect(() => {
+    if (containerRef.current && children) {
+      containerRef.current.innerHTML = "";
+      const childArray = Array.isArray(children) ? children : [children];
+      childArray.forEach((child) => {
+        const processedComponent = processDomComponent(child);
+        if (processedComponent instanceof HTMLElement) {
+          containerRef.current.appendChild(processedComponent);
         }
-    }, [children]);
-    return (
-        <div 
-        {...(engine && { "data-engine": engine})}
-        {...(wrapper && { "data-wrapper": wrapper})}
-          ref={containerRef}
-          style={{display : "contents"}}
-        ></div>
-    );
+      });
+    }
+  }, [children]);
+
+  return (
+    <div
+      {...(engine && { "data-engine": engine})}
+      {...(wrapper && { "data-wrapper": wrapper})}
+      ref={containerRef}
+      style={{ display: "contents" }}
+    />
+  );
 }
