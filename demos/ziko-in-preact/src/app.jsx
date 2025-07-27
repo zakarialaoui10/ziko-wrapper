f1ae7a7bb567f6782421e07102702b71396ea3f7
zakarialaoui10 <zakarialaoui10@gmail.com>
2025-07-27 11:18:20 +0100

    add links

--- demos/ziko-in-preact/src/app.jsx
+++ demos/ziko-in-preact/src/app.jsx
@@ -7,14 +7,15 @@ const Sketch = () => Canvas3D(
     )
     .useOrbitControls()
     .style({
-      outline : "3px darkblue solid"
+      outline : "3px darkblue solid",
+      cursor : "grab"
     })
 const Consigne = ({msg}) => p(msg);
 
 const Container = ({color}, ...children) => {
   return Flex(
     ...children
-  ).vertical(0, "space-around").size("80%").style({
+  ).vertical(-1, "space-around").size("80%").style({
     padding : "2em"
   })
 }
@@ -30,6 +31,14 @@ export const App = () =>{
          <Sketch />
         </Container>
       </ZikoWrapper>
+      <p>
+        Like the demo? Show support by starring the libraries behind it.<br/>
+        <a href="https://github.com/preactjs/preact">Preact</a><br/>
+        <a href="https://github.com/zakarialaoui10/zikojs">Zikojs</a><br/>
+        <a href="https://github.com/zakarialaoui10/ziko-wrapper">Ziko-Wrapper</a><br/>
+        <a href="https://github.com/zakarialaoui10/p5.wrapper">P5.wrapper</a><br/>
+        <a href="https://github.com/vitejs/vite">Vite</a><br/>
+      </p>
     </>
   )
 }
